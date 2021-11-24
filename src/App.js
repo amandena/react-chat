048aa9b833b3273d3a8df7ab712e0dbf67702aad
amandena <manda.m.johnson@gmail.com>
2021-11-23 22:09:19 -0500

    added chat engine credentials

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const App = () => {
     <div className='App'>
       <ChatEngine
         height='100vh'
-        projectID=''
-        userName=''
-        userSecret=''
+        projectID='2f404c36-a292-40e5-a1b9-9a6d84be940e'
+        userName='amandena'
+        userSecret='123234345'
       />
     </div>
   )
