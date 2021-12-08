7184b27a15ff0c08884487478a6b6c63b894e05a
amandena <manda.m.johnson@gmail.com>
2021-12-07 23:40:31 -0500

    added read receipts to chat feed component

--- src/components/ChatFeed.js
+++ src/components/ChatFeed.js
@@ -6,6 +6,16 @@ const ChatFeed = (props) => {
 
   const {chats, activeChat, userName, messages} = props
   const chat = chats && chats[activeChat]
+
+  const renderReadReceipts = (message, isMyMessage) => {
+    return chat.people.map((person, index) => person.last_read === message.id && (
+      <div
+        key={`read_${index}`}
+        className="read-receipt"
+        style={{float: isMyMessage ? 'right' : 'left', backgroundImage: `url(${person?.person?.avatar})`}}
+      />
+    ))
+  }
    
   const renderMessages = () => {
     const keys = Object.keys(messages)
@@ -19,7 +29,7 @@ const ChatFeed = (props) => {
             { isMyMessage ? <MyMessage message={message}/> : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/> }
           </div>
           <div className='read-receipts' style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
-            read-receipts
+            {renderReadReceipts(message, isMyMessage)}
           </div>
         </div>
       )
