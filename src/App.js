60225be70aeb0a757b8fb0f3fcc9570e939fb20e
amandena <manda.m.johnson@gmail.com>
2021-12-09 22:34:19 -0500

    conditionally rendering login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css'
 import { ChatEngine } from 'react-chat-engine' 
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm/>
+  
   return (
     <div className='App'>
       <ChatEngine
